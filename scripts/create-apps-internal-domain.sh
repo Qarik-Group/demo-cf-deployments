7d24ba8edf862c579fc7d5e07ebb92f2b5dfbdd0
patryk-kozak <thehackbee@gmail.com>
2022-07-20 15:16:07 +0000

    Add additional case for app.internal error handling

--- scripts/create-apps-internal-domain.sh
+++ scripts/create-apps-internal-domain.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cf create-shared-domain apps.internal --internal 2> error.txt
-cat error.txt | grep "The domain name is taken" | head -1 | xargs -r echo "Looks like domain exists, quitting with 0 code!" && exit 0
+cat error.txt | grep "The domain name is taken|is already in use" | head -1 | xargs -r echo "Looks like domain exists, quitting with 0 code!" && exit 0
 rm error.txt
