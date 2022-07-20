292332d60e265932a20fd70feedd7bb00b752b8d
patryk-kozak <thehackbee@gmail.com>
2022-07-20 15:22:53 +0000

    Show registered error from output

--- scripts/create-apps-internal-domain.sh
+++ scripts/create-apps-internal-domain.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cf create-shared-domain apps.internal --internal 2> error.txt
+more error.txt
 cat error.txt | grep "The domain name is taken|is already in use" | head -1 | xargs -r echo "Looks like domain exists, quitting with 0 code!" && exit 0
 rm error.txt
