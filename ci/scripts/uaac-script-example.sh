a1fa558db35b516576d6b8e652e4e3a668611486
patryk-kozak <thehackbee@gmail.com>
2022-08-02 09:12:08 +0000

    Dont spawn new shell, reuse exisiting

--- ci/scripts/uaac-script-example.sh
+++ ci/scripts/uaac-script-example.sh
@@ -1,5 +1,7 @@
-#LDAP, but can be changed with `--origin okta`` to OKTA ;)
+#!/usr/bin/env bash
+set -x
 uaac group add cloud_controller.admin_read_only
+#LDAP, but can be changed with `--origin okta`` to OKTA ;)
 uaac group map --name scim.read "CF_ENV_ADMIN"
 uaac group map --name scim.write "CF_ENV_ADMIN"
 uaac group map --name cloud_controller.admin "CF_ENV_ADMIN"
