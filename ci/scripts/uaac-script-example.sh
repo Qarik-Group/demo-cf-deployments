fe2a95a64c1f389720500318f544556a24b1af74
patryk-kozak <thehackbee@gmail.com>
2022-08-02 09:09:09 +0000

    Add UAAC script to mimic client-env behaviors

---
+++ ci/scripts/uaac-script-example.sh
@@ -0,0 +1,5 @@
+#LDAP, but can be changed with `--origin okta`` to OKTA ;)
+uaac group add cloud_controller.admin_read_only
+uaac group map --name scim.read "CF_ENV_ADMIN"
+uaac group map --name scim.write "CF_ENV_ADMIN"
+uaac group map --name cloud_controller.admin "CF_ENV_ADMIN"
